a734415da3ecfe757955cab57d790141596a0d46
pankaj952596 <pankajkumar952596@gmail.com>
2026-08-18 14:58:53 +0530

    Update stg1.tf

--- Practice2/stg1.tf
+++ Practice2/stg1.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "pooja ka storage"
+  name     = "TCSRG"
   location = "central india"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+resource "azurerm_storage_account" "example1" {
+  name                     = "storageaccountname1"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
