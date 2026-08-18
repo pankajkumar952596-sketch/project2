7f7a7d116cb7153adeba074fad22ae35737c96ad
pankaj952596 <pankajkumar952596@gmail.com>
2026-08-18 14:48:16 +0530

    changes

--- Practice2/stg1.tf
+++ Practice2/stg1.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "pooja ka storage"
+  location = "central india"
 }
 
 resource "azurerm_storage_account" "example" {
